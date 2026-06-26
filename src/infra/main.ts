b616ec0ab565d8c10fc66cb4380932a6f7d0c62b
arthur-viana <arthur.viana262@gmail.com>
2026-06-26 16:59:24 -0300

    Cors

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -7,6 +7,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors()
+
   const config = new DocumentBuilder()  
     .setTitle('Instagram da CT')
     .setDescription('Documentacao das rotas para o front-end')
